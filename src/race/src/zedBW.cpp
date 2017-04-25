43e3767814d466f2ae6aff31eebe29629c3dd150
Jetson <zainab.khan@cox.net>
2017-04-24 21:43:30 -0700

    Hough Transform in progress

--- src/race/src/zedBW.cpp
+++ src/race/src/zedBW.cpp
@@ -2,6 +2,9 @@
 #include <sensor_msgs/Image.h>
 #include <math.h>
 #define square(x) x*x
+#define THRESHOLD 100
+
+struct polar_point { int roh; int theta; };
 
 class zedBW
 {
@@ -11,24 +14,19 @@ class zedBW
   ros::Publisher pub_r_,pub_l_;
   ros::Subscriber sub_r_,sub_l_;
 
-
-  void callback_r(const sensor_msgs::Image& msg)
+  void callback(const sensor_msgs::Image& msg, sensor_msgs::Image& edge)
   {
-    sensor_msgs::Image edge;
     int i,j;
     int W = msg.width, H = msg.height;
-    if (msg.encoding.compare("bgr8")) {
-        printf("Incorrect Image format type (not currently able to handle other than 'bgr8')\n");
-        return;
-    }
+
     edge.header = msg.header;
     edge.height = msg.height;
     edge.width = msg.width;
     edge.encoding = "mono8"; // now single color (B/W)
     edge.is_bigendian = msg.is_bigendian;
     edge.step = msg.step/3; // only have 1/3 the data in bw (no R, G, B arrays)
+   
     std::vector <unsigned char> bw; // to hold raw bw
-    std::vector <double> edges; // to hold sgm 
     
     /* Consolidate color into black and white photo */
     for (i=0;i<H;i++) {
@@ -40,6 +38,7 @@ class zedBW
         }
     }
 
+    std::vector <double> edges; // to hold sgm 
     /* Calculate dx, dy, sgm */
     int dx,dy;
     double max = 0;
@@ -62,81 +61,98 @@ class zedBW
             edges.push_back(0); // right column
         }
     }
+    bw.clear();
 
+    std::vector <unsigned char> edge_img;
     /* Scale edge (sgm) data to 0 to 255 range */
-    for (i=0;i<edges.size();i++) edge.data.push_back(edges[i]/max*255);
-    
-    /* free up data, in case */
+    for (i=0;i<edges.size();i++) edge_img.push_back((edges[i]/max*255 > THRESHOLD)?255 :0);
     edges.clear();
-    bw.clear(); 
 
+    //std::vector <polar_point> voting;
+    std::vector <int> voting[180];
+
+    /* Hough Transform */
+    int theta, roh, roh_min, roh_max;
+    for (i=0;i<H;i++) {
+        for (j=0;j<W;j++) {
+            int x,y;
+            x = j;
+            y = H - 1 - i;
+            int index = W*i + j;
+            if (edge_img[index]) {
+                for (theta=0;theta<180;theta++) {
+                    roh = -x*sin(theta*M_PI/180) + y*cos(theta*M_PI/180);
+                    roh_min = (roh_min>roh)? roh: roh_min;
+                    roh_max = (roh_max<roh)? roh: roh_max;
+                    voting[theta].push_back(roh);    
+                }
+            } 
+        }
+    }
+    printf("max %d, min %d\n",roh_max, roh_min);
+
+    edge.data.swap(edge_img);
+    
+    edge_img.clear();   
+    /* 
+
+    int hough_max = 0;
+    int final_r, final_t;
+    for (theta=0;theta<180;theta++) {
+        for (roh=roh_min;roh<roh_max+1;roh++) {
+            if (count(voting,theta,roh)>hough_max) {
+                hough_max = count(voting,theta,roh);
+                final_r = roh;
+                final_t = theta;
+            }
+        }
+    }
+
+    printf("r %d, t %d\n", roh, theta);
+
+    for (i=0;i<H;i++) {
+        for (j=0;j<W;j++) {
+            int x = j;
+            int y = H-1-i;
+            if (abs(x*sin(final_t*M_PI/180) - y*cos(final_t*M_PI/180) + final_r) < 1) edge.data.push_back(255);
+            else edge.data.push_back(0);
+        }
+    }
+    */
+
+  }
+
+  int count(std::vector <int> * voting, int theta, int roh) {
+    int i,n;
+    for (i=0;i<voting[theta].size();i++) if (voting[theta][i]==roh) n++;
+    return n;
+  }
+
+
+  void callback_r(const sensor_msgs::Image& msg)
+  {
+    if (msg.encoding.compare("bgr8")) {
+        printf("Incorrect Image format type (not currently able to handle other than 'bgr8')\n");
+        return;
+    }
+    sensor_msgs::Image edge;
+    callback(msg,edge); // do operations on data
     pub_r_.publish(edge); 
     
   }
 
   void callback_l(const sensor_msgs::Image& msg)
   {
-    sensor_msgs::Image edge;
-    int i,j;
-    int W = msg.width, H = msg.height;
     if (msg.encoding.compare("bgr8")) {
         printf("Incorrect Image format type (not currently able to handle other than 'bgr8')\n");
         return;
     }
-    edge.header = msg.header;
-    edge.height = msg.height;
-    edge.width = msg.width;
-    edge.encoding = "mono8"; // now single color (B/W)
-    edge.is_bigendian = msg.is_bigendian;
-    edge.step = msg.step/3; // only have 1/3 the data in bw (no R, G, B arrays)
-    std::vector <unsigned char> bw; // to hold raw bw
-    std::vector <double> edges; // to hold sgm 
-    
-    /* Consolidate color into black and white photo */
-    for (i=0;i<H;i++) {
-        for (j=0;j<W;j++) {
-            long sum = 0;
-            int k;
-            for (k=-1;k<2;k++) sum += msg.data[3*W*i + 3*j +k];
-            bw.push_back(sum/3);
-        }
-    }
-
-    /* Calculate dx, dy, sgm */
-    int dx,dy;
-    double max = 0;
-    for (i=0;i<H;i++) {
-        if (i==0 || i==(H-1)) { // top/bottom row
-            for (j=0;j<W;j++) edges.push_back(0);
-        }
-        else {
-            edges.push_back(0); // left column
-            for (j=1;j<W-1;j++) {
-                int index = W*i + j; 
-                dx = bw[index+W+1] + 2*bw[index+1] + bw[index-W+1] \
-                - (bw[index+W-1] + 2*bw[index-1] + bw[index-W-1]);
-                dy = bw[index-W-1] + 2*bw[index-W] + bw[index-W+1]\
-                - (bw[index+W-1] + 2*bw[index+W] + bw[index+W+1]);
-                double sgm = sqrt(square(dx) + square(dy));
-                max = (max<sgm)? sgm: max;
-                edges.push_back(sgm);
-            }
-            edges.push_back(0); // right column
-        }
-    }
-
-    /* Scale edge (sgm) data to 0 to 255 range */
-    for (i=0;i<edges.size();i++) edge.data.push_back(edges[i]/max*255);
-    
-    /* free up data, in case */
-    edges.clear();
-    bw.clear(); 
-
+    sensor_msgs::Image edge;
+    callback(msg,edge); // do operations on data
     pub_l_.publish(edge); 
     
   }
-
-
+  
   public:
   zedBW():nh_(), private_nh_("~")
   {

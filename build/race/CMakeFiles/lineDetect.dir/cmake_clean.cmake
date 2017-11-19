0862a04812a307afef4d4781f44a31340db3515d
Xenocidel <aliao4@uci.edu>
2017-11-19 13:09:35 -0800

    missed some new files

---
+++ build/race/CMakeFiles/lineDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lineDetect.dir/src/lineDetector.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/race/lineDetect.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/race/lineDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lineDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
